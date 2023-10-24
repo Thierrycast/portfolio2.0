bbf6e5ed7128b0b3047a50e5047a7b9154eac731
Thierry Castro <thierrycastro555@gmail.com>
2023-10-24 14:41:51 -0300

    feat(aboutsection): criar seção sobre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from './page.module.css'
 
 import Header from '@/components/Header'
 import HomeSection from '@/components/HomeSection'
+import AboutSection from '@/components/AboutSection'
 
 export default function Home() {
   return (
     <>
       <Header/>
       <HomeSection/>
+      <AboutSection/>
     </>
   )
 }
---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,35 @@
+import styles from './aboutSection.module.scss'
+
+export default function index() {
+  return (
+    <div className={styles.container} >
+         <h1>Sobre( )</h1>
+         <p>Olá, meu nome é <span>Thierry Barreto de Castro</span>, tenho 20 anos, moro em Sardoá-MG e sou <span>Desenvolvedor de Software Fullstack</span>. Sempre tive contato com tecnologia e nos últimos anos tenho me identificado bastante com essa área, o que me levou a estudar programação.</p>
+         <h2>Trajetória:</h2>
+
+        <div className={styles.trajetoriaContainer} >
+        <div className={styles.trajetoria} >
+            <div className={styles.linha}>
+                <div className={styles.bolinha}>
+                    <span></span>
+                    <p>Desenvolvimento de Software Fullstack Cubos Academy</p>
+                </div>
+                <div className={styles.bolinha}>
+                    <span></span>
+                    <p>Análise e Desenvolvimento de Sistemas (ADS)</p>
+                </div>
+                <div className={styles.bolinha}>
+                    <span></span>
+                    <p>Entrevistador Técnico Cubos Academy</p>
+                </div>
+                <div className={styles.bolinha}>
+                    <span></span>
+                    <p>Desenvolvedor Freelancer</p>
+                </div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+  )
+}
