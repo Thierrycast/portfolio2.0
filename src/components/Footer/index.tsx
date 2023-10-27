2701abad35363a128094131ed3e1cf89a73790d5
Thierry Castro <thierrycastro555@gmail.com>
2023-10-27 15:27:08 -0300

    feat(footer): criar footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import Styles from './footer.module.scss'
+
+export default function Footer() {
+  return (
+    <div className={Styles.container} >
+      <p>Copyright © 2023 - Todos os direitos reservados - Thierry Castro</p>
+    </div>
+  )
+}
