a60bb29ce131cc38156baf1a5d49db321c846db7
Thierry Castro <thierrycastro555@gmail.com>
2023-10-27 16:56:45 -0300

    feat: adição de links ao projeto

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -2,7 +2,7 @@ import styles from './aboutSection.module.scss'
 
 export default function index() {
   return (
-    <div className={styles.container} >
+    <div id='about' className={styles.container} >
          <h1>Sobre( )</h1>
          <p>Olá, meu nome é <span>Thierry Barreto de Castro</span>, tenho 20 anos, moro em Sardoá-MG e sou <span>Desenvolvedor de Software Fullstack</span>. Sempre tive contato com tecnologia e nos últimos anos tenho me identificado bastante com essa área, o que me levou a estudar programação.</p>
          <h2>Trajetória:</h2>
--- src/components/HomeSection/index.tsx
+++ src/components/HomeSection/index.tsx
@@ -2,7 +2,7 @@ import styles from './homeSection.module.scss'
 
 export default function HomeSection() {
   return (
-    <section className={styles.container} >
+    <section id='home' className={styles.container} >
         <div className={styles.left_div} >
             <p>Olá, eu sou</p>
             <h2>Thierry Castro</h2>
@@ -13,7 +13,7 @@ export default function HomeSection() {
 
         <div className={styles.right_div}  >
             <p>Aqui você vai encontrar um pouco do meu <span>trabalho</span> e meus <span>contatos</span>.</p>
-            <button>Currículo</button>
+            <a target='_blank' href='https://docs.google.com/document/d/1la5h_fkWi6YI20JM0pwotjAA0AErxRdxC3LB2lEQDyM/edit?usp=sharing' >Currículo</a>
         </div>
     </section>
   )
