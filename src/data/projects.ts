685e9da5880647a81a0b70e2be4d806523979ddb
Thierry Castro <thierrycastro555@gmail.com>
2023-11-07 12:06:43 -0300

    style: estilos do menu

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+      name: "Projeto Fuja",
+      description: "Desenvolvimento de um site para uma empresa de audiovisual, utilizando React, TypeScript e responsividade.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: '../../assets/jobs/fuja.png'
+    },
+    {
+      name: "Portfólio Fotografo",
+      description: "Criação de um portfólio online para um fotógrafo, com o uso de Next.js, TypeScript e Sass.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: "Imagem: Insira a URL aqui" 
+    },
+    {
+      name: "APIs de Tarefas",
+      description: "Desenvolvimento de três APIs com o mesmo propósito: gerenciar tarefas. Feitas em TypeScript, C# e Java.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: "Imagem: Insira a URL aqui" 
+    },
+    {
+      name: "Projeto Fuja",
+      description: "Desenvolvimento de um site para uma empresa de audiovisual, utilizando React, TypeScript e responsividade.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: "Imagem: Insira a URL aqui" 
+    },
+    {
+      name: "Portfólio Fotografo",
+      description: "Criação de um portfólio online para um fotógrafo, com o uso de Next.js, TypeScript e Sass.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: "Imagem: Insira a URL aqui" 
+    },
+    {
+      name: "APIs de Tarefas",
+      description: "Desenvolvimento de três APIs com o mesmo propósito: gerenciar tarefas. Feitas em TypeScript, C# e Java.",
+      repo: "Repositório: Insira a URL aqui", 
+      site: "Site: Insira a URL aqui", 
+      img: "Imagem: Insira a URL aqui" 
+    },
+  ];
