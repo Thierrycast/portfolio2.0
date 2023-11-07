85d1043c6b59b0089a02c4092791032459b36b22
Thierry Castro <thierrycastro555@gmail.com>
2023-11-07 12:06:43 -0300

    style: estilos do menu

--- src/components/projectsSection/index.tsx
+++ src/components/projectsSection/index.tsx
@@ -1,56 +1,7 @@
 'use client'
 
-import index from '../AboutSection'
 import styles from './projectsSection.module.scss'
-
-import fuja from '../../assets/jobs/fuja.png'
-
-const projects = [
-  {
-    name: "Projeto Fuja",
-    description: "Desenvolvimento de um site para uma empresa de audiovisual, utilizando React, TypeScript e responsividade.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: '../../assets/jobs/fuja.png'
-  },
-  {
-    name: "Portfólio Fotografo",
-    description: "Criação de um portfólio online para um fotógrafo, com o uso de Next.js, TypeScript e Sass.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: "Imagem: Insira a URL aqui" 
-  },
-  {
-    name: "APIs de Tarefas",
-    description: "Desenvolvimento de três APIs com o mesmo propósito: gerenciar tarefas. Feitas em TypeScript, C# e Java.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: "Imagem: Insira a URL aqui" 
-  },
-  {
-    name: "Projeto Fuja",
-    description: "Desenvolvimento de um site para uma empresa de audiovisual, utilizando React, TypeScript e responsividade.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: "Imagem: Insira a URL aqui" 
-  },
-  {
-    name: "Portfólio Fotografo",
-    description: "Criação de um portfólio online para um fotógrafo, com o uso de Next.js, TypeScript e Sass.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: "Imagem: Insira a URL aqui" 
-  },
-  {
-    name: "APIs de Tarefas",
-    description: "Desenvolvimento de três APIs com o mesmo propósito: gerenciar tarefas. Feitas em TypeScript, C# e Java.",
-    repo: "Repositório: Insira a URL aqui", 
-    site: "Site: Insira a URL aqui", 
-    img: "Imagem: Insira a URL aqui" 
-  },
-];
-
-
+import {projects} from '@/data/projects'
 
 export default function ProjectsSection() {
 
