a617cb69fe1cb694dd3a95c6accc20b2e52a8fdd
Thierry Castro <thierrycastro555@gmail.com>
2023-11-09 18:33:10 -0300

    refactor: atualizando texto sobre

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -4,7 +4,13 @@ export default function index() {
   return (
     <div id='about' className={styles.container} >
          <h1>Sobre( )</h1>
-         <p className={styles.about_text} >Olá, meu nome é <span>Thierry Barreto de Castro</span>, tenho 20 anos, moro em Sardoá-MG e sou <span>Desenvolvedor de Software Fullstack</span>. Sempre tive contato com tecnologia e nos últimos anos tenho me identificado bastante com essa área, o que me levou a estudar programação.</p>
+         {/* <p className={styles.about_text} >Olá, meu nome é <span>Thierry Barreto de Castro</span>, tenho 20 anos, moro em Sardoá-MG e sou <span>Desenvolvedor de Software Fullstack</span>. Sempre tive contato com tecnologia e nos últimos anos tenho me identificado bastante com essa área, o que me levou a estudar programação.</p> */}
+
+         <p className={styles.about_text}>
+            Meu nome é <span>Thierry Barreto de Castro</span>, tenho 20 anos, e sou um <span>desenvolvedor de software fullstack</span>.<br/><br/> Atualmente, estou cursando Análise e Desenvolvimento de Sistemas (ADS) e aprimorando meus estudos em novas tecnologias e linguagens. Estou sempre em busca de novas oportunidades para aprender e crescer, e estou ansioso para contribuir para o desenvolvimento de produtos inovadores que possam impactar positivamente a vida das pessoas.
+        </p>
+
+
          <h2>Trajetória:</h2>
 
         <div className={styles.trajetoriaContainer} >
