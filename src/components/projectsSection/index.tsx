97091bd51912062929436955d3eb87e176547531
Thierry Barreto de Castro <81525885+Thierrycast@users.noreply.github.com>
2023-11-09 19:27:48 -0300

    Update link

--- src/components/projectsSection/index.tsx
+++ src/components/projectsSection/index.tsx
@@ -28,7 +28,7 @@ export default function ProjectsSection() {
 
       ))}
       </div>
-      <span><a target='_blank' href=''>Repositórios no GitHub</a></span>
+      <span><a target='_blank' href='https://github.com/Thierrycast?tab=repositories'>Repositórios no GitHub</a></span>
 
     </div>
   )
